a7fd85f27e44f6204f0757f9a979483168b7f44c
Rodrigo - Front-end <34324071+frontrgomes@users.noreply.github.com>
2022-10-19 12:46:03 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
+// Faça o consumo da API abaixo (retornará uma lista com 14 animes)
+// Mostre na tela todos os animes na seguinte ordem: . primeiramente dragon_ball e naruto (não importa a ordem entre os dois) . depois o restante da listagem sem os dois primeiros já mostrados anteriormente
+// Cada anime mostrado na tela deve ter o título e a imagem
+// CSS de forma que desejar
+
 
 async function loadPost() {
     document.getElementById("post").innerHTML = 'Carregando...';
-    let req =  await fetch('https://anime-facts-rest-api.herokuapp.com/api/v1')
+    let req =  await fetch('https://anime-facts-rest-api.herokuapp.com/api/v1');
     let json = await req.json();
-    montarPokemon(json.data);
+
+    const data = json.data;
+
+    const dbzEnaruto = [];
+    const outros = [];
+    
+    data.forEach(anime => {
+      if(anime.anime_name === 'naruto' || anime.anime_name === 'dragon_ball') {
+        dbzEnaruto.push(anime);
+      }else {
+       outros.push(anime);
+      }
+    })
+    
+    montarAnime([...dbzEnaruto, ...outros]); 
 }
 
-function montarPokemon (lista) {
+function montarAnime (lista) {
+
     let html = '';
     for(let i in lista){
         html += `
-            <div class='card-pokemon'>
+            <div class='card-anime'>
                 <div class='image'>
                     <img src="${lista[i].anime_img}"/>
                 </div>
@@ -24,6 +44,3 @@ function montarPokemon (lista) {
 }
 loadPost();
 
-// $(document).ready(function() {
-   
-// });
